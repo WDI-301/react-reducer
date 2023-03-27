2d65fd3f0d960fbca5b4239956d07b62bd05c9b2
Michael Dwyer <michael.dwyer@codeimmersives.com>
2023-03-27 11:59:03 -0700

    refactored into useReducer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React, { useState } from 'react'  
+import React, { useState, useReducer } from 'react'  
 import logo from './logo.svg';
 import './App.css';
 
+
+const counterReducer = (counter, action) => {
+  console.log(action)
+  switch (action.type) {
+      case 'Increment':
+        return counter+1
+      case 'Decrement':
+        return counter-1
+      case 'Add':
+        return counter+action.inputHandler
+      case 'Subtract':
+        return counter-action.inputHandler
+      case 'Divide':
+        return counter / action.inputHandler
+      case 'Multiply':
+        return counter * action.inputHandler
+      case 'Power' :
+        return counter ** action.inputHandler
+      case 'Remainder':
+        return counter % action.inputHandler 
+      default:
+        alert("Not a Math function")
+        break;
+    }
+}
+
+
 function App() {
-  const [counter, setCounter] = useState(0)
+
+  // const [counter, setCounter] = useState(0)
+  let initialCounter = 0
+  const [counter, dispatch] = useReducer( counterReducer, initialCounter )
   const [inputHandler, setInputHandler] = useState(10)
 
   const mathArr = ['Increment', 'Decrement', 'Add','Subtract','Multiply', 'Divide', 'Power', 'Remainder']
-  const counterFuncAdd = () => {
-    setCounter(counter+1)
-  }
+  // const counterFuncAdd = () => {
+  //   setCounter(counter+1)
+  // }
 
-  const counterFuncSubtract = () => {
-    setCounter(counter-1)
-  }
+  // const counterFuncSubtract = () => {
+  //   setCounter(counter-1)
+  // }
 
-  const counterFuncParam = (type) => {
-    type === 'Add' && setCounter(counter+1)
-    type === 'Subtract' && setCounter(counter-1)
-    type === 'Divide' && setCounter(counter / 2)
-    type === 'Multiply' && setCounter(counter * 2)
-  }
+  // const counterFuncParam = (type) => {
+  //   type === 'Add' && setCounter(counter+1)
+  //   type === 'Subtract' && setCounter(counter-1)
+  //   type === 'Divide' && setCounter(counter / 2)
+  //   type === 'Multiply' && setCounter(counter * 2)
+  // }
   // const counterFuncInput = (type) => {
   //   type === 'Add' && setCounter(counter+inputHandler)
   //   type === 'Subtract' && setCounter(counter-inputHandler)
@@ -28,35 +58,10 @@ function App() {
   //   type === 'Multiply' && setCounter(counter * inputHandler)
   // }
     const counterFuncInput = (type) => {
-      switch (type) {
-        case 'Increment':
-          setCounter(counter+1)
-          break;
-        case 'Decrement':
-          setCounter(counter-1)
-          break;
-        case 'Add':
-          setCounter(counter+inputHandler)
-          break;
-        case 'Subtract':
-          setCounter(counter-inputHandler)
-          break;
-        case 'Divide':
-          setCounter(counter / inputHandler)
-          break;
-        case 'Multiply':
-          setCounter(counter * inputHandler)
-          break;
-        case 'Power' :
-          setCounter(counter ** inputHandler)
-          break;
-        case 'Remainder':
-          setCounter(counter % inputHandler)
-          break; 
-        default:
-          alert("Not a Math function")
-          break;
-      }
+      dispatch({
+        type: type,
+        inputHandler: inputHandler
+      })
     }
 
 
@@ -64,15 +69,15 @@ function App() {
   return (
     <div className="App App-header">
       <h1>Counter: {counter}</h1>
-      <button onClick={() => setCounter(0)}>Reset</button>
+      {/* <button onClick={() => setCounter(0)}>Reset</button> */}
       {/* 
       <button onClick={counterFuncAdd}>Add 1</button>
       <button onClick={counterFuncSubtract}>Subtracttract 1</button> 
       */}
-      <button onClick={() => counterFuncParam('Add')}>Add 1</button>
+      {/* <button onClick={() => counterFuncParam('Add')}>Add 1</button>
       <button onClick={() => counterFuncParam('Subtract')}>Subtracttract 1</button>
       <button onClick={() => counterFuncParam('Divide')}>Divide by 2</button>
-      <button onClick={() => counterFuncParam('Multiply')}>Multiply by 2</button>
+      <button onClick={() => counterFuncParam('Multiply')}>Multiply by 2</button> */}
       <p>--------------------------------------------------------------------</p>
       <input type='number' 
         onChange={(e)=>setInputHandler(parseInt(e.target.value))} 
